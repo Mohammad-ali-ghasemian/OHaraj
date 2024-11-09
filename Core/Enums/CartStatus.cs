73e73258b9f0a30a24cecd33a931701921239a89
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-11-09 11:14:24 +0330

    CartStatus Enum

---
+++ Core/Enums/CartStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OHaraj.Core.Enums
+{
+    public enum CartStatus
+    {
+        [Display(Name = "لغوشده")]
+        Cancellation,
+        [Display(Name = "پرداخت نشده")]
+        Unpaid,
+        [Display(Name = "درحال پرداخت")]
+        PendingPayment,
+        [Display(Name = "پرداخت شده")]
+        Paid
+    }
+}
--- obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
+++ obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f11956af5a5b07fc4df7ccaddd1dc3c4d244292")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a83024b74487580a154499ffe9d3f210459796")]
 [assembly: System.Reflection.AssemblyProductAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
