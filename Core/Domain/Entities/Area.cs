7f0f0e0e7cd71a906e1c7ccd2a272f94304740cc
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-11-09 11:33:31 +0330

    Area entity

---
+++ Core/Domain/Entities/Area.cs
@@ -0,0 +1,8 @@
+﻿namespace OHaraj.Core.Domain.Entities
+{
+    public class Area
+    {
+        public int Id { get; set; } 
+        public string Description { get; set; } = string.Empty;
+    }
+}
