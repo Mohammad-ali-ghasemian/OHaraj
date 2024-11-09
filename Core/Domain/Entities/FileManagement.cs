8de44eecdaecb42cb16cdfdb0f1bfcec6927bf5e
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-11-09 10:42:25 +0330

    FileManagement Entity

---
+++ Core/Domain/Entities/FileManagement.cs
@@ -0,0 +1,15 @@
+﻿using OHaraj.Core.Enums;
+
+namespace OHaraj.Core.Domain.Entities
+{
+    public class FileManagement
+    {
+        public int Id { get; set; }
+        public byte[] Content { get; set; } = Array.Empty<byte>();
+        public FileType Type { get; set; }
+        public string path { get; set; } = string.Empty;
+
+        //Navigation Properties
+
+    }
+}
