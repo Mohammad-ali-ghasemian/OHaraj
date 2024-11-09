1aa6ea85e971f3b214a69db5b456b0b9884b1574
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-11-09 11:03:00 +0330

    Category Entity

---
+++ Core/Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace OHaraj.Core.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+
+        // Navigation Properties
+        Product
+        Model
+        FileManagementId
+    }
+}
