417633d579c3296b8046d14832a41c8066e3fca2
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-11-09 11:21:59 +0330

    ProductComment entity

---
+++ Core/Domain/Entities/ProductComment.cs
@@ -0,0 +1,12 @@
+﻿namespace OHaraj.Core.Domain.Entities
+{
+    public class ProductComment
+    {
+        public int Id { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public bool IsApproved { get; set; }
+
+        // Navigation Properties
+    }
+}
