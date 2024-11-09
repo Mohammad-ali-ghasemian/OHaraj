53dd517bbd71c4d291d4bf8643f547bdb45a3a72
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-11-09 11:15:07 +0330

    Cart Entity

---
+++ Core/Domain/Entities/Cart.cs
@@ -0,0 +1,16 @@
+﻿using OHaraj.Core.Enums;
+
+namespace OHaraj.Core.Domain.Entities
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime? DatePaid { get; set; }
+        public int TotalAmount { get; set; }
+        public CartStatus Status { get; set; }
+
+        // Navigation Properties
+        UserId
+    }
+}
