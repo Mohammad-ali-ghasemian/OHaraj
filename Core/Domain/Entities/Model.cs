39a83024b74487580a154499ffe9d3f210459796
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-11-09 11:03:11 +0330

    Model Entity

---
+++ Core/Domain/Entities/Model.cs
@@ -0,0 +1,14 @@
+﻿namespace OHaraj.Core.Domain.Entities
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+
+        // Navigation Properties
+        Product
+        Category
+        FileManagement
+    }
+}
--- obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
+++ obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de44eecdaecb42cb16cdfdb0f1bfcec6927bf5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f11956af5a5b07fc4df7ccaddd1dc3c4d244292")]
 [assembly: System.Reflection.AssemblyProductAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
