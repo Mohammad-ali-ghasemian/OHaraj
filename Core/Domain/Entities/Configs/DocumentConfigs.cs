3b9867384e12da7c330baffbe99e36547844f28e
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-11-19 15:12:25 +0330

    DocumentCinfigs  Navigation Properties

--- Core/Domain/Entities/Configs/DocumentConfigs.cs
+++ Core/Domain/Entities/Configs/DocumentConfigs.cs
@@ -1,8 +1,13 @@
-﻿namespace OHaraj.Core.Domain.Entities.Configs
+﻿using OHaraj.Core.Domain.Entities.Settings;
+
+namespace OHaraj.Core.Domain.Entities.Configs
 {
     public class DocumentConfigs
     {
         public int Id { get; set; }
         public int MaxSize { get; set; }
+
+        // Navigation Properties
+        public List<DocumentSettings> DocumentSettings { get; set; }
     }
 }
