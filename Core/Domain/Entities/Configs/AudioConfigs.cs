f5e59de2be90e009a66667070bf9a43814200155
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-11-19 15:12:15 +0330

    AudioCinfigs  Navigation Properties

--- Core/Domain/Entities/Configs/AudioConfigs.cs
+++ Core/Domain/Entities/Configs/AudioConfigs.cs
@@ -1,9 +1,14 @@
-﻿namespace OHaraj.Core.Domain.Entities.Configs
+﻿using OHaraj.Core.Domain.Entities.Settings;
+
+namespace OHaraj.Core.Domain.Entities.Configs
 {
     public class AudioConfigs
     {
         public int Id { get; set; }
         public int MaxSize { get; set; }
         public int MaxLength { get; set; }
+
+        // Navigation Properties
+        public List<AudioSettings> AudioSettings { get; set; }
     }
 }
