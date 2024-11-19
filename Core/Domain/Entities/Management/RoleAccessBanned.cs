e26ec7c14d6983ae93d1a0d400956bf8e78231e8
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-11-19 15:14:00 +0330

    RoleBannedAccess Navigation Properties

--- Core/Domain/Entities/Management/RoleAccessBanned.cs
+++ Core/Domain/Entities/Management/RoleAccessBanned.cs
@@ -1,11 +1,16 @@
-﻿namespace OHaraj.Core.Domain.Entities.Handling
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OHaraj.Core.Domain.Entities.Handling
 {
     public class RoleAccessBanned
     {
         public int Id { get; set; }
 
         // Navigation Properties
-        RoleId
-        MenuId
+        public IdentityRole Role { get; set; }
+        public string RoleId { get; set; }
+
+        public List<Menu> Menu {  get; set; } 
+        public int MenuId { get; set; }
     }
 }
