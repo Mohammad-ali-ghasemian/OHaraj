c18e9610262849925ac1f3207a30c579abcb6619
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-11-19 15:13:07 +0330

    DocumentSettings  Navigation Properties

--- Core/Domain/Entities/Settings/DocumentSettings.cs
+++ Core/Domain/Entities/Settings/DocumentSettings.cs
@@ -1,12 +1,20 @@
-﻿namespace OHaraj.Core.Domain.Entities.Settings
+﻿using OHaraj.Core.Domain.Entities.Configs;
+using OHaraj.Core.Domain.Entities.Handling;
+using OHaraj.Core.Enums;
+
+namespace OHaraj.Core.Domain.Entities.Settings
 {
     public class DocumentSettings
     {
         public int Id { get; set; }
+        public Area Area { get; set; }
 
         // Navigation Properties
+
+        public DocumentConfigs DocumentConfigs { get; set; }
+        public int DocumentConfigsId { get; set; }
+
+        public Menu Menu { get; set; }
         public int MenuId { get; set; }
-        public int AreaId { get; set; }
-        public int ConfigId { get; set; }
     }
 }
