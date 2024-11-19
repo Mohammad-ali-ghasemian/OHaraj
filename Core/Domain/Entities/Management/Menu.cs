974a9a8a77f5b16c86ad14bd0eb0e3702db6c1d7
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-11-19 15:13:50 +0330

    Menu Navigation Properties

--- Core/Domain/Entities/Management/Menu.cs
+++ Core/Domain/Entities/Management/Menu.cs
@@ -1,9 +1,24 @@
-﻿namespace OHaraj.Core.Domain.Entities.Handling
+﻿using OHaraj.Core.Domain.Entities.Settings;
+
+namespace OHaraj.Core.Domain.Entities.Handling
 {
     public class Menu
     {
         public int Id { get; set; }
-        public int ParentId { get; set; }
         public string Title { get; set; } = string.Empty;
+
+        // Navigation Properties
+        public Menu ParentMenu { get; set; }
+        public int ParentId { get; set; }
+
+        public List<RoleAccessBanned> RoleAccessBanned { get; set;}
+
+        public List<AudioSettings> AudioSettings { get; set;}
+
+        public List<DocumentSettings> DocumentSettings { get; set; }
+
+        public List<ImageSettings> ImageSettings { get; set; }
+
+        public List<VideoSettings> VideoSettings { get; set; }
     }
 }
