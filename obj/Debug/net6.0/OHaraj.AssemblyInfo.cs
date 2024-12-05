dbec1dc3f3596eb7de0df02a20a299bf045ed8d8
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-12-05 18:13:28 +0330

    Newtonsoft.json package

--- obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
+++ obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7949fa570f910341438892c303e7556b94ec6025")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3aee60946739ac9094e8e917a4647bf91d31a4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
