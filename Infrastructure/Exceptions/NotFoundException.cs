54e4ff2d1758009cf42116d01872a7204ecd9f52
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-12-05 18:14:23 +0330

    Create Exception classes

---
+++ Infrastructure/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace Project.Application.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string item) : base($"{item}")
+        {
+        }
+        public NotFoundException() : base("اطلاعات مورد نظر پیدا نشد")
+        {
+        }
+    }
+}
