7b7547a74682846f775276da18cdbe548c671f7a
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-12-05 18:28:49 +0330

    inject ExceptionMiddleware in program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using OHaraj.Infrastructure;
+using Project.Application.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
