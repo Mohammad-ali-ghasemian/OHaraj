da4840fd094ff12e18bce42df1c71c5fc4a2c128
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-12-05 09:23:15 +0330

    Update Menu.cs

--- Core/Domain/Entities/Management/Menu.cs
+++ Core/Domain/Entities/Management/Menu.cs
@@ -5,13 +5,12 @@ namespace OHaraj.Core.Domain.Entities.Handling
     public class Menu
     {
         public int Id { get; set; }
+        public int? ParentId { get; set; }
         public string Title { get; set; } = string.Empty;
 
         // Navigation Properties
-        public Menu ParentMenu { get; set; }
-        public int ParentId { get; set; }
 
-        public List<RoleAccessBanned> RoleAccessBanned { get; set;}
+        public RoleAccessBanned? RoleAccessBanned { get; set;}
 
         public List<AudioSettings> AudioSettings { get; set;}
 
@@ -20,5 +19,10 @@ namespace OHaraj.Core.Domain.Entities.Handling
         public List<ImageSettings> ImageSettings { get; set; }
 
         public List<VideoSettings> VideoSettings { get; set; }
+
+
+
+        public Menu? ParentMenu { get; set; }
+        public List<Menu>? SubMenus { get; set; }
     }
 }
