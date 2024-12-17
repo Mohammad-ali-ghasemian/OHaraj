35c0aab274cde6f493a2e0a1d53a9976d9bd2ac1
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-12-17 14:46:29 +0330

    Register Model

---
+++ Core/Domain/Models/Authentication/Register.cs
@@ -0,0 +1,26 @@
+﻿using DNTPersianUtils.Core;
+using Project.Application.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace OHaraj.Core.Domain.Models.Authentication
+{
+    public class Register
+    {
+        [Display(Name = "شماره تماس")]
+        [ValidIranianMobileNumber(ErrorMessage = PublicHelper.NotValidValidationErrorMessage)]
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public string MobileNumber { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [EmailAddress]
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
