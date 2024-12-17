479b5fb99b6c1a8774556b6d90c1194c89d0839d
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-12-17 14:46:37 +0330

    Login Model

---
+++ Core/Domain/Models/Authentication/Login.cs
@@ -0,0 +1,16 @@
+﻿using DNTPersianUtils.Core;
+using Project.Application.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace OHaraj.Core.Domain.Models.Authentication
+{
+    public class Login
+    {
+        [ValidIranianMobileNumber(ErrorMessage = PublicHelper.NotValidValidationErrorMessage)]
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public string MobileNumber { get; set; }
+
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public string Password { get; set; }
+    }
+}
