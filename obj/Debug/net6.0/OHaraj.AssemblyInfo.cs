415b6b1968162202e66b0f571bfc48047b90c4ad
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-01-12 20:21:38 +0330

    Create empty Authentication Controller

--- obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
+++ obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b7547a74682846f775276da18cdbe548c671f7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0534b68499126db77c0e5ae96b939af4aad5452")]
 [assembly: System.Reflection.AssemblyProductAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
