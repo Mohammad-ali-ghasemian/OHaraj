d3d5284294742d9a3df37af0747e625b8c647e3c
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-01-13 18:22:55 +0330

    Update AuthenticationRepository.cs

--- Repositories/AuthenticationRepository.cs
+++ Repositories/AuthenticationRepository.cs
@@ -4,5 +4,35 @@ namespace OHaraj.Repositories
 {
     public class AuthenticationRepository : IAuthenticationRepository
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        public AuthenticationRepository(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IdentityResult> AddUserAsync(IdentityUser user, string password)
+        {
+            return await _userManager.CreateAsync(user, password);
+        }
+
+        public Task<IEnumerable<IdentityUser>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityUser> GetUserAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IdentityUser>> GetUsersByRoleAsync(int roleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IdentityUser>> GetUsersByRoleAsync(string role)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
