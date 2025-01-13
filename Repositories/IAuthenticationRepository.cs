63055c535a2263567af22e2dbc3b9103c551bb58
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-01-13 21:38:17 +0330

    update Authentication Repository interface

--- Repositories/IAuthenticationRepository.cs
+++ Repositories/IAuthenticationRepository.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using OHaraj.Core.Domain.Models.Authentication;
 
 namespace OHaraj.Repositories
 {
     public interface IAuthenticationRepository
     {
         Task<IdentityResult> AddUserAsync(IdentityUser user, string password);
-        Task<IdentityUser> GetUserAsync(int userId);
+        Task<IdentityUser> GetUserAsync(string userId);
         Task<IEnumerable<IdentityUser>> GetAllUsersAsync();
-        Task<IEnumerable<IdentityUser>> GetUsersByRoleAsync(int roleId);
-        Task<IEnumerable<IdentityUser>> GetUsersByRoleAsync(string role);
+        Task<IEnumerable<IdentityUser>> GetUsersByRoleAsync(string roleName);
+        Task<SignInResult> SignInAsync(Login login);
     }
 }
