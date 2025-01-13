698045d10780d4c554f7bf37ee0af4fc7ddcdd26
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-01-13 18:22:49 +0330

    Update IAuthenticationRepository.cs

--- Repositories/IAuthenticationRepository.cs
+++ Repositories/IAuthenticationRepository.cs
@@ -4,5 +4,10 @@ namespace OHaraj.Repositories
 {
     public interface IAuthenticationRepository
     {
+        Task<IdentityResult> AddUserAsync(IdentityUser user, string password);
+        Task<IdentityUser> GetUserAsync(int userId);
+        Task<IEnumerable<IdentityUser>> GetAllUsersAsync();
+        Task<IEnumerable<IdentityUser>> GetUsersByRoleAsync(int roleId);
+        Task<IEnumerable<IdentityUser>> GetUsersByRoleAsync(string role);
     }
 }
