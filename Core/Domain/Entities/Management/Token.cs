8c5a9c910e4c606ca4ef50f6aff78f09df69c977
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-01-13 23:10:17 +0330

    Token Entity

---
+++ Core/Domain/Entities/Management/Token.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OHaraj.Core.Domain.Entities.Management
+{
+    public class Token
+    {
+        public int Id { get; set; }
+        public string? EmailVerificationToken { get; set; }
+        public DateTime? EmailVerifiedAt { get; set; }
+        public string? ResetPasswordToken { get; set; }
+        public DateTime? ResetPasswordTokenExpires { get; set; }
+
+        // Navigation properties
+        public IdentityUser User { get; set; }
+        public string UserId { get; set; } = string.Empty;
+    }
+}
