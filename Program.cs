8eed963a7d887dd97e30447f6f45c497a04f83f2
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-01-13 21:37:39 +0330

    add Cookie Setup to program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using OHaraj.Infrastructure;
@@ -6,6 +7,7 @@ using Project.Application.Middlewares;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// add database
 builder.Services.AddDbContext<ApplicationDbContext>
     (
     options => options.UseSqlServer(
@@ -14,6 +16,8 @@ builder.Services.AddDbContext<ApplicationDbContext>
     );
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Configure Password
 builder.Services.Configure<IdentityOptions>(options =>
 {
     options.Password.RequireDigit = false;
@@ -23,6 +27,15 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Password.RequiredLength = 6;
     options.Password.RequiredUniqueChars = 1;
 });
+
+// Cookie Setup
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.ExpireTimeSpan = TimeSpan.FromDays(10); // Cookie expires after 10 days
+        options.SlidingExpiration = true; // Resets expiration time if the user is active
+    });
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
