6b3a6d0309c035f6e9c6b0324a54ca78a23512de
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-01-14 21:23:41 +0330

    Update IAuthenticationService.cs

--- Core/Interfaces/Services/IAuthenticationService.cs
+++ Core/Interfaces/Services/IAuthenticationService.cs
@@ -1,6 +1,8 @@
-﻿namespace OHaraj.Core.Interfaces.Services
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OHaraj.Core.Interfaces.Services
 {
     public interface IAuthenticationService
-    {
+    
     }
 }
