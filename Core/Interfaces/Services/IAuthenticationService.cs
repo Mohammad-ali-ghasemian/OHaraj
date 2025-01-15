ab9a4ab38b887f67a8c7376107352e30e09be045
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-01-15 23:55:59 +0330

    IAuthenticationService: create raw methods

--- Core/Interfaces/Services/IAuthenticationService.cs
+++ Core/Interfaces/Services/IAuthenticationService.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using OHaraj.Core.Domain.Models.Authentication;
+using Project.Application.Responses;
 
 namespace OHaraj.Core.Interfaces.Services
 {
     public interface IAuthenticationService
-    
+    {
+        Task<ResponseStatus> Register(Register input);
+        Task<ResponseStatus> SendVerificationEmail(string email);// create token and add it to token table;
+        Task<string> VerifiyEmailToken(string email);// email verification in User table = true; verifiadAt in Token table;
+        Task<ResponseStatus> SendResetPasswordEmail(string email);// create token and add it to token table; create expire date;
+        Task<string> VerifiyResetPasswordToken(string email);
     }
 }
