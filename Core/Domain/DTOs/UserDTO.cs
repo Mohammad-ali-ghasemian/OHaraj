17dda1d7f4eac2060b345fcaf788f43d50412bd8
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-01-15 22:44:44 +0330

    Create UserDTO.cs

---
+++ Core/Domain/DTOs/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace OHaraj.Core.Domain.DTOs
+{
+    public class UserDTO
+    {
+        public string Id { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public bool? EmailConfirmed { get; set; }
+        public string? Role { get; set; }
+    }
+}
