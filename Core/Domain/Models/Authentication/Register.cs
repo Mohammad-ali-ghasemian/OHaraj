dc7a42a9f94fc92c26672908edf2423d2caa2250
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-01-30 18:31:30 +0330

    Update Register.cs

--- Core/Domain/Models/Authentication/Register.cs
+++ Core/Domain/Models/Authentication/Register.cs
@@ -13,6 +13,7 @@ namespace OHaraj.Core.Domain.Models.Authentication
 
         [Display(Name = "ایمیل")]
         [EmailAddress]
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
         public string? Email { get; set; }
 
         [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
