d1924eebd20fd0a4ef22429570a726a6d9b3923a
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-02-05 22:23:32 +0330

    VerifiyEmailToken IAuthenticationService

--- Core/Interfaces/Services/IAuthenticationService.cs
+++ Core/Interfaces/Services/IAuthenticationService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using OHaraj.Core.Domain.DTOs;
 using OHaraj.Core.Domain.Models.Authentication;
 using Project.Application.Responses;
 
@@ -6,11 +7,9 @@ namespace OHaraj.Core.Interfaces.Services
 {
     public interface IAuthenticationService
     {
-        Task<ResponseStatus> Register(Register input);
-        Task<ResponseStatus> SendVerificationEmail(string email);// create token and add it to token table;
-        Task<string> VerifiyEmailToken(string email);// email verification in User table = true; verifiadAt in Token table;
-        Task<ResponseStatus> SendResetPasswordEmail(string email);// create token and add it to token table; create expire date;
-        Task<string> VerifiyResetPasswordToken(string email);
         Task<ResponseStatus> Login(Login input);
+        Task<ResponseStatus> Register(Register input);
+        Task<ResponseStatus> SendVerificationEmail(string email);
+        Task<string> VerifiyEmailToken(string token);
     }
 }
