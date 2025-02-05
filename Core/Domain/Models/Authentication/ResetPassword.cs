a408d67e30123ea9ac8babaf2b49de6755647f9f
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-02-05 22:16:03 +0330

    ResetPassword Model

---
+++ Core/Domain/Models/Authentication/ResetPassword.cs
@@ -0,0 +1,18 @@
+﻿using Project.Application.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace OHaraj.Core.Domain.Models.Authentication
+{
+    public class ResetPassword
+    {
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public string Token { get; set; }
+    }
+}
