edea3720b3752b143fc7c1f3d2e85e8c3eea71ad
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-02-11 17:48:31 +0330

    Sign in & out IAdminRepository

--- Core/Interfaces/Repositories/IAdminRepository.cs
+++ Core/Interfaces/Repositories/IAdminRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace OHaraj.Core.Interfaces.Repositories
+﻿using Microsoft.AspNetCore.Identity;
+using OHaraj.Core.Domain.Models.Authentication;
+
+namespace OHaraj.Core.Interfaces.Repositories
 {
     public interface IAdminRepository
     {
+        Task<SignInResult> SignInAsync(Login login);
+        Task SignOutAsync();
     }
 }
