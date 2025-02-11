9fda175f30cdfd4060c012333bafb55e1b088927
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-02-11 17:38:38 +0330

    AdminService Cunstructor

--- Services/AdminService.cs
+++ Services/AdminService.cs
@@ -1,6 +1,30 @@
-﻿namespace OHaraj.Services
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using OHaraj.Core.Interfaces.Repositories;
+
+namespace OHaraj.Services
 {
     public class AdminService
     {
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IAuthenticationRepository _authenticationRepository;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AdminService(
+            IMapper mapper,
+            IHttpContextAccessor httpContextAccessor,
+            IAuthenticationRepository authenticationRepository,
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager
+            )
+        {
+            _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+            _authenticationRepository = authenticationRepository;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
     }
 }
