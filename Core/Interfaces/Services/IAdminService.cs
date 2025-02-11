ee50722d18c7ad24995b66edb5ec6fd80bce1aa2
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-02-11 17:38:13 +0330

    AddAdmin - UpdateAdmin - Login : IAdminService

--- Core/Interfaces/Services/IAdminService.cs
+++ Core/Interfaces/Services/IAdminService.cs
@@ -1,6 +1,12 @@
-﻿namespace OHaraj.Core.Interfaces.Services
+﻿using OHaraj.Core.Domain.DTOs;
+using OHaraj.Core.Domain.Models.Authentication;
+
+namespace OHaraj.Core.Interfaces.Services
 {
     public interface IAdminService
     {
+        Task<UserDTO> AddAdmin(Register input);
+        Task<UserDTO> UpdateAdmin();
+        Task<UserDTO> Login(Login input);
     }
 }
