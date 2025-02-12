2bd75038e0f81545c24903e2c0aee5da60a711d5
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-02-12 17:26:03 +0330

    Create AdminDTO.cs

---
+++ Core/Domain/DTOs/AdminDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace OHaraj.Core.Domain.DTOs
+{
+    public class AdminDTO
+    {
+        public string Id { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public IEnumerable<string>? Roles { get; set; }
+    }
+}
