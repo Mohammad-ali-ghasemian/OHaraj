ee8076af78f2e3e5c40740619e829924ba80b70d
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-02-12 17:37:49 +0330

    add more functions to IAdminRepository

--- Core/Interfaces/Repositories/IAdminRepository.cs
+++ Core/Interfaces/Repositories/IAdminRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using OHaraj.Core.Domain.Models.Authentication;
+using System.Security.Claims;
 
 namespace OHaraj.Core.Interfaces.Repositories
 {
@@ -7,5 +8,19 @@ namespace OHaraj.Core.Interfaces.Repositories
     {
         Task<SignInResult> SignInAsync(Login input);
         Task SignOutAsync();
+        Task<IdentityResult> AddAdmin(IdentityUser user, string password);
+        Task<IdentityResult> AddUserRolesAsync(IdentityUser user, IEnumerable<string> roles);
+        Task<IEnumerable<string>> GetUserRolesAsync(IdentityUser user);
+
+        Task<IdentityUser> GetUserByIdAsync(string userId);
+        Task<IdentityUser> GetUserByUsernameAsync(string username);
+        Task<IdentityUser> GetUserByEmailAsync(string email);
+        Task<IdentityUser> GetUserByPrincipalAsync(ClaimsPrincipal principal);
+        Task<IEnumerable<IdentityUser>> GetAllUsersAsync();
+        Task<IEnumerable<IdentityUser>> GetUsersByRoleAsync(string roleName);
+
+        Task<IdentityResult> UpdateUserAsync(IdentityUser user);
+
+        Task<IdentityResult> DeleteUserAsync(IdentityUser user);
     }
 }
