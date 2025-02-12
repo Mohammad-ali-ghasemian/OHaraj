6ed170e7c8482addfc3bd101d3db4f8e273e4fb9
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-02-13 01:44:16 +0330

    Update AdminDTO.cs

--- Core/Domain/DTOs/AdminDTO.cs
+++ Core/Domain/DTOs/AdminDTO.cs
@@ -5,6 +5,7 @@
         public string Id { get; set; }
         public string? Username { get; set; }
         public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
         public IEnumerable<string>? Roles { get; set; }
     }
 }
