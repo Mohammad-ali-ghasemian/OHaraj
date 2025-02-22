ce7209f9400f98332cc6f4cac0177c1b2fa63e88
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-02-22 18:04:56 +0330

    ProductService constructor

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,8 +1,26 @@
-﻿using OHaraj.Core.Interfaces.Services;
+﻿using AutoMapper;
+using OHaraj.Core.Interfaces.Repositories;
+using OHaraj.Core.Interfaces.Services;
 
 namespace OHaraj.Services
 {
     public class ProductService : IProductService
     {
+        private readonly IProductRepository _productRepository;
+        private readonly IAuthenticationRepository _authenticationRepository;
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public ProductService(
+            IProductRepository productRepository,
+            IAuthenticationRepository authenticationRepository,
+            IMapper mapper,
+            IHttpContextAccessor httpContextAccessor
+            )
+        {
+            _productRepository = productRepository;
+            _authenticationRepository = authenticationRepository;
+            _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+        }
     }
 }
