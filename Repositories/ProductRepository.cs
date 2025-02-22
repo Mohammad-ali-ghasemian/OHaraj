0edafa710651a7932a2a636624aa2efd12fe4228
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-02-22 18:04:48 +0330

    ProductRepository constructor

--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -1,8 +1,14 @@
 ﻿using OHaraj.Core.Interfaces.Repositories;
+using OHaraj.Infrastructure;
 
 namespace OHaraj.Repositories
 {
     public class ProductRepository : IProductRepository
     {
+        private readonly ApplicationDbContext _dbContext;
+        public ProductRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
     }
 }
