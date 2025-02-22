7562885a8a1d4fd7f2de8e4329eca7efc5d08d78
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-02-22 18:14:15 +0330

    donno

--- obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
+++ obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a893ad2e12b65c52b21116d1b520b134a9aa089")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce7209f9400f98332cc6f4cac0177c1b2fa63e88")]
 [assembly: System.Reflection.AssemblyProductAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
