89bf592fc47206812a191e1468f2f369bd510a5d
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-02-22 18:04:32 +0330

    ProductController constructor

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OHaraj.Core.Interfaces.Services;
+using Project.Application.Responses;
+using System.Net;
 
 namespace OHaraj.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ProducesResponseType(typeof(Response<string>), (int)HttpStatusCode.Unauthorized)]
+    [ProducesResponseType(typeof(Response<string>), (int)HttpStatusCode.BadRequest)]
     public class ProductController : ControllerBase
     {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
     }
 }
