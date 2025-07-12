7029d4ae666c47ecb51a90bb6e302707221bd4f8
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-07-13 00:36:16 +0330

    raw IProductRepository methods

--- Core/Interfaces/Repositories/IProductRepository.cs
+++ Core/Interfaces/Repositories/IProductRepository.cs
@@ -6,6 +6,12 @@ namespace OHaraj.Core.Interfaces.Repositories
     public interface IProductRepository
     {
         Task<ProductDTO> AddProductAsync(Product input);
-        
+        Task<ProductDTO> UpdateProductAsync(Product input);
+        Task<int> DeleteProductAsync(int id);
+        Task<ProductDTO> GetProductAsync(int id);
+        Task<IEnumerable<ProductDTO>> GetProductsAsync();
+        Task<IEnumerable<ProductDTO>> GetProductsByCategotyAsync(int categoryId);
+        Task<IEnumerable<ProductDTO>> GetProductsByModelAsync(int modelId);
+
     }
 }
