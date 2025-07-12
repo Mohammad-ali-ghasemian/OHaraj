df62d0e0398349a425f38e3f32b1a627394a77b6
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-07-13 01:00:36 +0330

    Update IProductRepository.cs

--- Core/Interfaces/Repositories/IProductRepository.cs
+++ Core/Interfaces/Repositories/IProductRepository.cs
@@ -5,9 +5,9 @@ namespace OHaraj.Core.Interfaces.Repositories
 {
     public interface IProductRepository
     {
-        Task<ProductDTO> AddProductAsync(Product input);
-        Task<ProductDTO> UpdateProductAsync(Product input);
-        Task<int> DeleteProductAsync(int id);
+        Task AddProductAsync(Product input);
+        Task UpdateProductAsync(Product input);
+        Task DeleteProductAsync(int id);
         Task<ProductDTO> GetProductAsync(int id);
         Task<IEnumerable<ProductDTO>> GetProductsAsync();
         Task<IEnumerable<ProductDTO>> GetProductsByCategotyAsync(int categoryId);
