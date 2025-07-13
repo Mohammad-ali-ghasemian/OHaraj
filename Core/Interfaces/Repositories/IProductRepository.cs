940040e4c5d12ff9173ea4cc49e4fc2d16a81083
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-07-13 23:08:59 +0330

    Update IProductRepository.cs

--- Core/Interfaces/Repositories/IProductRepository.cs
+++ Core/Interfaces/Repositories/IProductRepository.cs
@@ -8,30 +8,37 @@ namespace OHaraj.Core.Interfaces.Repositories
         Task AddProductAsync(Product input);
         Task UpdateProductAsync(Product input);
         Task DeleteProductAsync(Product input);
+
         Task<Product> GetProductAsync(int id);
         Task<IEnumerable<Product>> GetProductsAsync();
         Task<IEnumerable<Product>> GetProductsByCategotyAsync(int categoryId);
         Task<IEnumerable<Product>> GetProductsByModelAsync(int modelId);
-
-
-
-
-
         Task<IEnumerable<Product>> GetDeactiveProductsAsync();
         Task<IEnumerable<Product>> GetActiveProductsAsync();
+
+
+
+
         Task<ProductLike> IsLikedByUser(ProductLike input);
         Task<ProductComment> GetCommentAsync(int commentId);
-        Task<IEnumerable<ProductComment>> GetProductFiveNewestCommentsAsync(int courseId);
-        Task<IEnumerable<ProductComment>> GetProductTenNewestCommentsAsync(int courseId);
-        Task<IEnumerable<ProductComment>> GetProductAllVerifiedCommentsAsync(int courseId);
-        Task<IEnumerable<ProductComment>> GetProductAllCommentsAsync(int courseId);
-        Task<IEnumerable<ProductComment>> GetUserAllCommentsAsync(int userId);
-        Task<IEnumerable<ProductComment>> GetAllUnverifiedCommentsAsync();
-        Task<IEnumerable<ProductComment>> GetProductUnverifiedCommentsAsync(int courseId);
-        Task<IEnumerable<ProductComment>> GetUserUnverifiedCommentsAsync(int userId);
+        Task AddProductCommentAsync(ProductComment comment);
         Task UpdateProductCommentAsync(ProductComment comment);
         Task DeleteProductCommentAsync(ProductComment comment);
         Task DeleteAllProductCommentsAsync(IEnumerable<ProductComment> comments);
+
+        
+        Task<IEnumerable<ProductComment>> GetProductFiveNewestCommentsAsync(int courseId);
+        Task<IEnumerable<ProductComment>> GetProductTenNewestCommentsAsync(int courseId);
+        Task<IEnumerable<ProductComment>> GetProductAllVerifiedCommentsAsync(int courseId);
+        Task<IEnumerable<ProductComment>> GetProductUnverifiedCommentsAsync(int courseId);
+        Task<IEnumerable<ProductComment>> GetProductAllCommentsAsync(int courseId);
+
+
+        Task<IEnumerable<ProductComment>> GetAllUnverifiedCommentsAsync();
+
+        Task<IEnumerable<ProductComment>> GetUserAllCommentsAsync(int userId);
+        Task<IEnumerable<ProductComment>> GetUserUnverifiedCommentsAsync(int userId);
+        
 
     }
 }
