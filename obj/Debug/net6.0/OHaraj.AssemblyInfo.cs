3c9faf4db5ea2595268e38febc18ccddf202e42d
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-07-14 21:48:15 +0330

    install SixLabors package

--- obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
+++ obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3618d46d81ec17b2d95b326792f1e9b1453f71f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35809bf7a929f1a9db59ca7ca27bf122dfefd695")]
 [assembly: System.Reflection.AssemblyProductAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
