e1c9d05c17f86c90c4b9c67f61b31bcde1909d0f
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-07-14 21:42:32 +0330

    Create IFileStorageService.cs

---
+++ Infrastructure/FileManagers/IFileStorageService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Project.Application.Contracts.Infrastructure
+{
+    public interface IFileStorageService
+    {
+        Task DeleteFile(string containerName, string fileRoute);
+        Task<string> SaveFile(string containerName, IFormFile file, bool wattermark = false);
+        Task<string> EditFile(string containerName, IFormFile file, string fileRoute);
+    }
+}
