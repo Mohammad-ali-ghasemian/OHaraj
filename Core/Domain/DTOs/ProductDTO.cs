6d107f65bc2e2e72087340aa710eb6c7827ce17b
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-07-16 00:37:58 +0330

    Update ProductDTO.cs

--- Core/Domain/DTOs/ProductDTO.cs
+++ Core/Domain/DTOs/ProductDTO.cs
@@ -2,5 +2,18 @@
 {
     public class ProductDTO
     {
+        public string Name { get; set; }
+        public int? Weight { get; set; }
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+        public int? Length { get; set; }
+        public int Quantity { get; set; }
+        public string? ShortContent { get; set; }
+        public string? Content { get; set; }
+        public string Price { get; set; }
+        public int? DiscountPercent { get; set; } = 0;
+        public bool IsActive { get; set; } = false;
+        public int CategoryId { get; set; }
+        public int ModelId { get; set; }
     }
 }
