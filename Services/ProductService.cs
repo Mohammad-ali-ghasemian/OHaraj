7d0fce55fb975ed203debfb2068b88ac87105794
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-07-21 00:57:59 +0330

    UpdateProduct ProductService

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -17,7 +17,7 @@ namespace OHaraj.Services
         private readonly IAuthenticationRepository _authenticationRepository;
         private readonly IMapper _mapper;
         private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly string Usercontainer;
+        private readonly string FileContainer;
         private readonly IFileStorageService _uploaderService;
         public ProductService(
             IProductRepository productRepository,
@@ -31,7 +31,7 @@ namespace OHaraj.Services
             _authenticationRepository = authenticationRepository;
             _mapper = mapper;
             _httpContextAccessor = httpContextAccessor;
-            Usercontainer = "Product";
+            FileContainer = "Product";
             _uploaderService = uploaderService;
         }
 
@@ -55,7 +55,7 @@ namespace OHaraj.Services
                 fileId = await _productRepository.AddFileToTableAsync(new FileManagement
                 {
                     Type = Core.Enums.FileType.Image,
-                    path = await _uploaderService.SaveFile(Usercontainer, input.MainImage, wattermark: true);
+                    path = await _uploaderService.SaveFile(FileContainer, input.MainImage, wattermark: true)
                 });
             }
 
@@ -69,7 +69,7 @@ namespace OHaraj.Services
                     imgId = await _productRepository.AddFileToTableAsync(new FileManagement
                     {
                         Type = Core.Enums.FileType.Image,
-                        path = await _uploaderService.SaveFile(Usercontainer, image, wattermark: true)
+                        path = await _uploaderService.SaveFile(FileContainer, image, wattermark: true)
                     });
 
                     otherImages.Add(new ProductImages
@@ -107,6 +107,81 @@ namespace OHaraj.Services
 
         }
 
+        public async Task<ProductDTO> UpdateProduct(UpdateProduct input)
+        {
+            var product = await _productRepository.GetProductAsync(input.Id);
+            if (product == null)
+            {
+                throw new NotFoundException("محصول یافت نشد");
+            }
+
+            // update product main image from "FileManagement" table and static folder
+            var mainFile = await _productRepository.GetFileToTableAsync(product.FileManagementId);
+
+            int? fileId = null;
+            if (input.MainImage != null)
+            {
+                mainFile.path = await _uploaderService.EditFile(FileContainer, input.MainImage, mainFile.path);
+                fileId = await _productRepository.UpdateFileToTableAsync(mainFile);
+            }
+            else
+            {
+                await _uploaderService.DeleteFile(FileContainer, mainFile.path);
+                await _productRepository.DeleteFileToTableAsync(mainFile);
+            }
+
+            // update product other images from "FileManagement" table and static folder
+            foreach (var image in product.ProductImages)
+            {
+                var x = await _productRepository.GetFileToTableAsync(image.FileManagementId);
+                await _productRepository.DeleteFileToTableAsync(x);
+                await _uploaderService.DeleteFile(FileContainer, x.path);
+            }
+
+            List<ProductImages>? otherImages = null;
+            if (input.OtherImages != null)
+            {
+                int imgId;
+                int pivot = 1;
+                foreach (var image in input.OtherImages)
+                {
+                    imgId = await _productRepository.AddFileToTableAsync(new FileManagement
+                    {
+                        Type = Core.Enums.FileType.Image,
+                        path = await _uploaderService.SaveFile(FileContainer, image, wattermark: true)
+                    });
+
+                    otherImages.Add(new ProductImages
+                    {
+                        Order = pivot,
+                        FileManagementId = imgId
+                    });
+                    ++pivot;
+                }
+            }
+
+            product.Name = input.Name;
+            product.Weight = input.Weight;
+            product.Width = input.Width;
+            product.Height = input.Height;
+            product.Length = input.Length;
+            product.Quantity = input.Quantity;
+            product.ShortContent = input.ShortContent;
+            product.Content = input.Content;
+            product.Price = input.Price;
+            product.DiscountPercent = input.DiscountPercent;
+            product.IsActive = input.IsActive;
+            product.CategoryId = input.CategoryId;
+            product.ModelId = input.ModelId;
+            product.FileManagementId = fileId;
+            product.ProductImages = otherImages;
+
+            await _productRepository.UpdateProductAsync(product);
+
+            return _mapper.Map<ProductDTO>(product);
+
+        }
+
         public async Task<int> DeleteProduct(int productId)
         {
             var product = await _productRepository.GetProductAsync(productId);
@@ -130,11 +205,6 @@ namespace OHaraj.Services
         }
 
         public Task<IEnumerable<ProductDTO>> GetProductsByCategory(int categoryId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ProductDTO> UpdateProduct(UpsertProduct input)
         {
             throw new NotImplementedException();
         }
