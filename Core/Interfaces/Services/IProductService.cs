96da270990a3ea141c73a6ce9bd2c0b20e84f36f
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-07-21 01:00:18 +0330

    Update IProductService.cs

--- Core/Interfaces/Services/IProductService.cs
+++ Core/Interfaces/Services/IProductService.cs
@@ -6,7 +6,7 @@ namespace OHaraj.Core.Interfaces.Services
     public interface IProductService
     {
         Task<ProductDTO> AddProduct(UpsertProduct input);
-        Task<ProductDTO> UpdateProduct(UpsertProduct input);
+        Task<ProductDTO> UpdateProduct(UpdateProduct input);
         Task<ProductDTO> GetProduct(int productId);
         Task<IEnumerable<ProductDTO>> GetAllProducts();
         Task<IEnumerable<ProductDTO>> GetProductsByCategory(int categoryId);
