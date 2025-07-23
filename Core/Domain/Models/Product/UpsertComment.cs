90cd33e269f9692e603b454ad0d44fd6c9921a45
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-07-23 19:14:56 +0330

    Create UpsertComment.cs

---
+++ Core/Domain/Models/Product/UpsertComment.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OHaraj.Core.Domain.Models.Product
+{
+    public class UpsertComment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public int ProductId { get; set; }
+    }
+}
