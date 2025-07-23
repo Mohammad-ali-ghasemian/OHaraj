15e043361fd9217e642aa2d41302f4bfa427638e
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-07-23 19:29:28 +0330

    Update UpsertComment.cs

--- Core/Domain/Models/Product/UpsertComment.cs
+++ Core/Domain/Models/Product/UpsertComment.cs
@@ -4,7 +4,7 @@ namespace OHaraj.Core.Domain.Models.Product
 {
     public class UpsertComment
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
         public string Text { get; set; } = string.Empty;
         public int ProductId { get; set; }
     }
