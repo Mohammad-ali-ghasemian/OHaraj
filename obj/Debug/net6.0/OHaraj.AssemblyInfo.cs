a08898a58aa27971dd62f395c045414848b80a72
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-08-17 19:26:51 +0330

    install asp.net https certificate

--- obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
+++ obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5a0ca7d7ea082e41bd387c7d4a1ec1a6bcb448d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dbe77d705253e348f1d39864492a3111c95d5f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
