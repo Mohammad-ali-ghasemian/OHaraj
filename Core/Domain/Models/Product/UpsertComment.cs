f9ef5a7ad260a89c715bf482f81a5234f8d5a915
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-09-28 15:24:48 +0330

    Update UpsertComment.cs

--- Core/Domain/Models/Product/UpsertComment.cs
+++ Core/Domain/Models/Product/UpsertComment.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Project.Application.Helpers;
+using System.ComponentModel.DataAnnotations;
 
 namespace OHaraj.Core.Domain.Models.Product
 {
     public class  UpsertComment
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
         public string Text { get; set; } = string.Empty;
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
         public int ProductId { get; set; }
     }
 }
