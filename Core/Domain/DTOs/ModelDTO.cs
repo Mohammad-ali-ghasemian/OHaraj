2ea649e41b114b299e08c22b5d58ac439a9e0ab6
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-09-30 18:40:10 +0330

    Update ModelDTO.cs

--- Core/Domain/DTOs/ModelDTO.cs
+++ Core/Domain/DTOs/ModelDTO.cs
@@ -1,6 +1,18 @@
-﻿namespace OHaraj.Core.Domain.DTOs
+﻿using Project.Application.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace OHaraj.Core.Domain.DTOs
 {
     public class ModelDTO
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string? ImagePath { get; set; }
     }
 }
