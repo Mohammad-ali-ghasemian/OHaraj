15de8dc0f3ff91a535e9515d6f3f237d1461f58f
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-09-30 15:36:24 +0330

    Create UpsertCategory.cs

---
+++ Core/Domain/Models/Product/UpsertCategory.cs
@@ -0,0 +1,14 @@
+﻿using Project.Application.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace OHaraj.Core.Domain.Models.Product
+{
+    public class UpsertCategory
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int ParentCategoryId { get; set; }
+    }
+}
