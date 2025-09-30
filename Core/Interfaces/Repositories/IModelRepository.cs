87f09e5c53eb2924993279ed436186f3c42e0067
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-09-30 18:40:02 +0330

    Update IModelRepository.cs

--- Core/Interfaces/Repositories/IModelRepository.cs
+++ Core/Interfaces/Repositories/IModelRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace OHaraj.Core.Interfaces.Repositories
+﻿using OHaraj.Core.Domain.Entities.Handling;
+using OHaraj.Core.Domain.Entities.Shop;
+
+namespace OHaraj.Core.Interfaces.Repositories
 {
-    public class IModelRepository
+    public interface IModelRepository
     {
+        Task AddModelAsync(Model input);
+        Task UpdateModelAsync(Model input);
+        Task DeleteModelAsync(Model input);
+
+        Task<Model> GetModelAsync(int id);
+        Task<IEnumerable<Model>> GetModelsAsync();
+
+        Task<int> AddFileToTableAsync(FileManagement input);
+        Task<int> UpdateFileToTableAsync(FileManagement input);
+        Task<FileManagement> GetFileToTableAsync(int? fileId);
+        Task<int> DeleteFileToTableAsync(FileManagement input);
     }
 }
