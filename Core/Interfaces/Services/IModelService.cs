f6db5a93ff103016c3f11cf2f40019a0374f8ff8
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-09-30 18:39:58 +0330

    Update IModelService.cs

--- Core/Interfaces/Services/IModelService.cs
+++ Core/Interfaces/Services/IModelService.cs
@@ -1,6 +1,14 @@
-﻿namespace OHaraj.Core.Interfaces.Services
+﻿using OHaraj.Core.Domain.DTOs;
+using OHaraj.Core.Domain.Models.Product;
+
+namespace OHaraj.Core.Interfaces.Services
 {
-    public class IModelService
+    public interface IModelService
     {
+        Task<ModelDTO> AddModel(UpsertModel input);
+        Task<ModelDTO> UpdateModel(UpsertModel input);
+        Task<int> DeleteModel(int modelId);
+        Task<ModelDTO> GetModel(int modelId);
+        Task<IEnumerable<ModelDTO>> GetAllModels();
     }
 }
