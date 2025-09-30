1b29e2226ca5f449f896d63ad933a4dd708ce996
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-09-30 15:43:53 +0330

    Create ICategoryService.cs

---
+++ Core/Interfaces/Services/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using OHaraj.Core.Domain.DTOs;
+using OHaraj.Core.Domain.Models.Product;
+
+namespace OHaraj.Core.Interfaces.Services
+{
+    public interface ICategoryService
+    {
+        Task<CategoryDTO> AddCategory(UpsertCategory input);
+        Task<CategoryDTO> UpdateCategory(UpsertCategory input);
+        Task<int> DeleteCategory(int CategoryId);
+        Task<CategoryDTO> GetCategory(int CategoryId);
+        Task<IEnumerable<CategoryDTO>> GetAllCategories(string filter = null);
+    }
+}
