34aad5143a2b73067df141df1c93bb9e382ac027
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-09-30 18:39:53 +0330

    Update ModelRepository.cs

--- Repositories/ModelRepository.cs
+++ Repositories/ModelRepository.cs
@@ -1,6 +1,71 @@
-﻿namespace OHaraj.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using OHaraj.Core.Domain.Entities.Handling;
+using OHaraj.Core.Domain.Entities.Shop;
+using OHaraj.Core.Interfaces.Repositories;
+using OHaraj.Infrastructure;
+
+namespace OHaraj.Repositories
 {
-    public class ModelRepository
+    public class ModelRepository : IModelRepository
     {
+        private readonly ApplicationDbContext _dbContext;
+        public ModelRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext; 
+        }
+
+        public async Task AddModelAsync(Model input)
+        {
+            await _dbContext.AddAsync(input);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateModelAsync(Model input)
+        {
+            _dbContext.Update(input);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteModelAsync(Model input)
+        {
+            _dbContext.Remove(input);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Model> GetModelAsync(int id)
+        {
+            return await _dbContext.Models.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<IEnumerable<Model>> GetModelsAsync()
+        {
+            return await _dbContext.Models.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<int> AddFileToTableAsync(FileManagement input)
+        {
+            await _dbContext.AddAsync(input);
+            await _dbContext.SaveChangesAsync();
+            return input.Id;
+        }
+
+        public async Task<int> UpdateFileToTableAsync(FileManagement input)
+        {
+            _dbContext.Update(input);
+            await _dbContext.SaveChangesAsync();
+            return input.Id;
+        }
+
+        public async Task<FileManagement> GetFileToTableAsync(int? fileId)
+        {
+            return await _dbContext.FileManagement.FirstOrDefaultAsync(x => x.Id == fileId);
+        }
+
+        public async Task<int> DeleteFileToTableAsync(FileManagement input)
+        {
+            _dbContext.Remove(input);
+            await _dbContext.SaveChangesAsync();
+            return input.Id;
+        }
     }
 }
