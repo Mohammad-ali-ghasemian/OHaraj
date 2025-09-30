1a92585923420a861e17f365e9b30bd5c4e5d678
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-09-30 15:43:50 +0330

    Create ICategoryRepository.cs

---
+++ Core/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using OHaraj.Core.Domain.Entities.Handling;
+using OHaraj.Core.Domain.Entities.Shop;
+
+namespace OHaraj.Core.Interfaces.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task AddCategoryAsync(Category input);
+        Task UpdateCategoryAsync(Category input);
+        Task DeleteCategoryAsync(Category input);
+
+        Task<Category> GetCategoryAsync(int id);
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+
+        Task<int> AddFileToTableAsync(FileManagement input);
+        Task<int> UpdateFileToTableAsync(FileManagement input);
+        Task<FileManagement> GetFileToTableAsync(int? fileId);
+        Task<int> DeleteFileToTableAsync(FileManagement input);
+    }
+}
