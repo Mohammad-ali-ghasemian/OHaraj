fefc887b404d4859baef61b34f26c1b90f2c89a5
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-09-30 18:40:06 +0330

    Update UpsertModel.cs

--- Core/Domain/Models/Product/UpsertModel.cs
+++ Core/Domain/Models/Product/UpsertModel.cs
@@ -1,6 +1,21 @@
-﻿namespace OHaraj.Core.Domain.Models.Product
+﻿using OHaraj.Core.Domain.Entities.Shop;
+using Project.Application.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace OHaraj.Core.Domain.Models.Product
 {
     public class UpsertModel
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public int CategoryId { get; set; }
+
+        public IFormFile? Image { get; set; }
     }
 }
