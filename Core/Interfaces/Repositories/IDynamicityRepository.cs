df1f3e7fb179c96fee1e60ecf09a32d254f5283b
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-04 23:26:26 +0330

    configs

--- Core/Interfaces/Repositories/IDynamicityRepository.cs
+++ Core/Interfaces/Repositories/IDynamicityRepository.cs
@@ -28,5 +28,36 @@ namespace OHaraj.Core.Interfaces.Repositories
 
 
 
+        //Configs
+        Task<int> AddImageConfigAsync(ImageConfigs input);
+        Task<int> UpdateImageConfigAsync(ImageConfigs input);
+        Task<int> DeleteImageConfigAsync(ImageConfigs input);
+
+        Task<ImageConfigs> GetImageConfigAsync(int id);
+        Task<IEnumerable<ImageConfigs>> GetImageConfigsAsync();
+
+
+        Task<int> AddVideoConfigAsync(VideoConfigs input);
+        Task<int> UpdateVideoConfigAsync(VideoConfigs input);
+        Task<int> DeleteVideoConfigAsync(VideoConfigs input);
+
+        Task<VideoConfigs> GetVideoConfigAsync(int id);
+        Task<IEnumerable<VideoConfigs>> GetVideoConfigsAsync();
+
+
+        Task<int> AddAudioConfigAsync(AudioConfigs input);
+        Task<int> UpdateAudioConfigAsync(AudioConfigs input);
+        Task<int> DeleteAudioConfigAsync(AudioConfigs input);
+
+        Task<AudioConfigs> GetAudioConfigAsync(int id);
+        Task<IEnumerable<AudioConfigs>> GetAudioConfigsAsync();
+
+
+        Task<int> AddDocumentConfigAsync(DocumentConfigs input);
+        Task<int> UpdateDocumentConfigAsync(DocumentConfigs input);
+        Task<int> DeleteDocumentConfigAsync(DocumentConfigs input);
+
+        Task<DocumentConfigs> GetDocumentConfigAsync(int id);
+        Task<IEnumerable<DocumentConfigs>> GetDocumentConfigsAsync();
     }
 }
