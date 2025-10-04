7848956619aa29ded614e06b64d9d8418c36a738
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-04 23:32:42 +0330

    basic controller

--- Controllers/DynamicityController.cs
+++ Controllers/DynamicityController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OHaraj.Core.Domain.Entities.Handling;
 using OHaraj.Core.Interfaces.Services;
 using Project.Application.Responses;
 using System.Net;
 
 namespace OHaraj.Controllers
 {
+    [Authorize(Roles = "Admin,SuperAdmin")]
     [Route("api/[controller]")]
     [ApiController]
     [ProducesResponseType(typeof(Response<string>), (int)HttpStatusCode.Unauthorized)]
@@ -16,6 +19,13 @@ namespace OHaraj.Controllers
         public DynamicityController(IDynamicityService dynamicityService)
         {
             _dynamicityService = dynamicityService;
+        }
+
+        [HttpPost("Add-Menu")]
+        [Produces(typeof(Response<Menu>))]
+        public async Task<IActionResult> AddMenu(Menu input)
+        {
+
         }
     }
 }
