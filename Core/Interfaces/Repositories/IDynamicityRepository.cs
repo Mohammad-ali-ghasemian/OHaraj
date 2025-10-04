51f1d683582c0f008a80a12f299beb93c359a4d6
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-04 23:26:04 +0330

    Menu

--- Core/Interfaces/Repositories/IDynamicityRepository.cs
+++ Core/Interfaces/Repositories/IDynamicityRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace OHaraj.Core.Interfaces.Repositories
+﻿using OHaraj.Core.Domain.Entities.Configs;
+using OHaraj.Core.Domain.Entities.Handling;
+
+namespace OHaraj.Core.Interfaces.Repositories
 {
     public interface IDynamicityRepository
     {
+        //Menu
+        Task<int> AddMenuAsync(Menu input);
+        Task<int> UpdateMenuAsync(Menu input);
+        Task<int> DeleteMenuAsync(Menu input);
+
+        Task<Menu> GetMenuAsync(int id);
+        Task<IEnumerable<Menu>> GetMenusAsync();
+        // first get all menus then remove these access denied menus and send
+        Task<IEnumerable<Menu>> GetAccessDeniedMenusAsync(IEnumerable<string> roleIds);
+
+
+
     }
 }
