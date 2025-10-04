1faebeb1f3e6dd6a3f67bd73007aad5ab4655163
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-04 20:33:26 +0330

    initial DynamicityController

--- obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
+++ obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+844f7fb884298783a6b9b6474c0c18dc7ada95f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f084bb9cb61ac521bfed520800a2ea517fa5d487")]
 [assembly: System.Reflection.AssemblyProductAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
