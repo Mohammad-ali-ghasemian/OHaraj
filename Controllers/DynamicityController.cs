0f0b955f267dd616e864390467917eba7b4228fa
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-04 20:35:18 +0330

    Elementary setup

--- Controllers/DynamicityController.cs
+++ Controllers/DynamicityController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OHaraj.Core.Interfaces.Services;
+using Project.Application.Responses;
+using System.Net;
 
 namespace OHaraj.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ProducesResponseType(typeof(Response<string>), (int)HttpStatusCode.Unauthorized)]
+    [ProducesResponseType(typeof(Response<string>), (int)HttpStatusCode.BadRequest)]
     public class DynamicityController : ControllerBase
     {
+        private readonly IDynamicityService _dynamicityService;
+        public DynamicityController(IDynamicityService dynamicityService)
+        {
+            _dynamicityService = dynamicityService;
+        }
     }
 }
