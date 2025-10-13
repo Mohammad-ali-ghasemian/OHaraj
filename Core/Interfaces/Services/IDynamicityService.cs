decda10a5b6d53cddc7b2a96d4ce20979ba123ba
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-13 20:13:38 +0330

    Update IDynamicityService.cs

--- Core/Interfaces/Services/IDynamicityService.cs
+++ Core/Interfaces/Services/IDynamicityService.cs
@@ -6,6 +6,7 @@ namespace OHaraj.Core.Interfaces.Services
 {
     public interface IDynamicityService
     {
+        //Menu
         Task<int> UpsertMenu(UpsertMenu input);
         Task<int> DeleteMenu(int menuId);
         Task<Menu> GetMenu(int menuId);
@@ -13,15 +14,36 @@ namespace OHaraj.Core.Interfaces.Services
         Task<IEnumerable<Menu>> GetMyAccessMenus();
         Task<IEnumerable<Menu>> GetOtherAccessMenus(string userId);
 
+
+
+        //Role Access Banned
         Task<int> UpsertAccessBan(UpsertRoleAccessBanned input);
         Task<int> DeleteAccessBan(int accessBanId);
         Task<RoleAccessBanned> GetAccessBan(int id);
         Task<IEnumerable<RoleAccessBanned>> GetAccessBan(string userId);
         Task<IEnumerable<RoleAccessBanned>> GetAllAccessBans();
 
-        Task<IEnumerable<ImageConfigs>> UpsertImageConfig(UpsertImageConfig input);
+
+
+        //Configs
+        Task<int> UpsertImageConfig(UpsertImageConfig input);
         Task<int> DeleteImageConfig(int imageConfigId);
         Task<ImageConfigs> GetImageConfig(int imageConfigId);
         Task<IEnumerable<ImageConfigs>> GetImageConfigs();
+
+        Task<int> UpsertVideoConfig(UpsertVideoConfig input);
+        Task<int> DeleteVideoConfig(int videoConfigId);
+        Task<VideoConfigs> GetVideoConfig(int videoConfigId);
+        Task<IEnumerable<VideoConfigs>> GetVideoConfigs();
+
+        Task<int> UpsertAudioConfig(UpsertAudioConfig input);
+        Task<int> DeleteAudioConfig(int audioConfigId);
+        Task<AudioConfigs> GetAudioConfig(int audioConfigId);
+        Task<IEnumerable<AudioConfigs>> GetAudioConfigs();
+
+        Task<int> UpsertDocumentConfig(UpsertDocumentConfig input);
+        Task<int> DeleteDocumentConfig(int documentConfigId);
+        Task<DocumentConfigs> GetDocumentConfig(int documentConfigId);
+        Task<IEnumerable<DocumentConfigs>> GetDocumentConfigs();
     }
 }
