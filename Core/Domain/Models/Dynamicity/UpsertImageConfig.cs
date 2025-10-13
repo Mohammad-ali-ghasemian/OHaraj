acf936e265ba81659ff158120adf9fe98e2767ea
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-13 15:17:30 +0330

    Create UpsertImageConfig.cs

---
+++ Core/Domain/Models/Dynamicity/UpsertImageConfig.cs
@@ -0,0 +1,16 @@
+﻿using Project.Application.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace OHaraj.Core.Domain.Models.Dynamicity
+{
+    public class UpsertImageConfig
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public int? MaxSize { get; set; }
+        public int? MaxWidth { get; set; }
+        public int? MaxHeight { get; set; }
+    }
+}
