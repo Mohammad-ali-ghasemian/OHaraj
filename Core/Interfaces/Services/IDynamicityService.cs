ce92c9053a8142cebb6e2e5c3be6864a83b80b8a
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-13 15:11:13 +0330

    Update IDynamicityService.cs

--- Core/Interfaces/Services/IDynamicityService.cs
+++ Core/Interfaces/Services/IDynamicityService.cs
@@ -6,8 +6,14 @@ namespace OHaraj.Core.Interfaces.Services
     public interface IDynamicityService
     {
         Task<int> UpsertMenu(UpsertMenu input);
+        Task<int> DeleteMenu(int menuId);
         Task<Menu> GetMenu(int menuId);
         Task<IEnumerable<Menu>> GetMenus();
+        Task<IEnumerable<Menu>> GetMyAccessMenus();
+        Task<IEnumerable<Menu>> GetOtherAccessMenus(string userId);
+
+        Task<int> UpsertAccessBan(UpsertRoleAccessBanned input);
+        Task<int> DeleteAccessBan(int accessBanId);
 
     }
 }
