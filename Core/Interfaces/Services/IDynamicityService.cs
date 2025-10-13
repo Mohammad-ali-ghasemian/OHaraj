2200a293388d80f861563a5ebcb7467390ba812e
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-13 20:26:11 +0330

    Update IDynamicityService.cs

--- Core/Interfaces/Services/IDynamicityService.cs
+++ Core/Interfaces/Services/IDynamicityService.cs
@@ -1,6 +1,9 @@
 ﻿using OHaraj.Core.Domain.Entities.Configs;
 using OHaraj.Core.Domain.Entities.Management;
+using OHaraj.Core.Domain.Entities.Settings;
 using OHaraj.Core.Domain.Models.Dynamicity;
+using OHaraj.Core.Domain.Models.Dynamicity.Configs;
+using OHaraj.Core.Enums;
 
 namespace OHaraj.Core.Interfaces.Services
 {
@@ -45,5 +48,40 @@ namespace OHaraj.Core.Interfaces.Services
         Task<int> DeleteDocumentConfig(int documentConfigId);
         Task<DocumentConfigs> GetDocumentConfig(int documentConfigId);
         Task<IEnumerable<DocumentConfigs>> GetDocumentConfigs();
+
+
+
+        //Settings
+        Task<int> UpsertImageSetting(UpsertSetting input);
+        Task<int> DeleteImageSetting(int imageSettingId);
+        Task<ImageSettings> GetImageSetting(int imageSettingId);
+        Task<IEnumerable<ImageSettings>> GetImageSettings();
+        Task<IEnumerable<ImageSettings>> GetImageSettingsByMenuId(int menuId);
+        Task<IEnumerable<ImageSettings>> GetImageSettingsByArea(Area area);
+        Task<IEnumerable<ImageSettings>> GetImageSettingsByConfigId(int imageConfigId);
+
+        Task<int> UpsertVideoSetting(UpsertSetting input);
+        Task<int> DeleteVideoSetting(int videoSettingId);
+        Task<VideoSettings> GetVideoSetting(int videoSettingId);
+        Task<IEnumerable<VideoSettings>> GetVideoSettings();
+        Task<IEnumerable<VideoSettings>> GetVideoSettingsByMenuId(int menuId);
+        Task<IEnumerable<VideoSettings>> GetVideoSettingsByArea(Area area);
+        Task<IEnumerable<VideoSettings>> GetVideoSettingsByConfigId(int videoConfigId);
+
+        Task<int> UpsertAudioSetting(UpsertSetting input);
+        Task<int> DeleteAudioSetting(int audioSettingId);
+        Task<AudioSettings> GetAudioSetting(int audioSettingId);
+        Task<IEnumerable<AudioSettings>> GetAudioSettings();
+        Task<IEnumerable<AudioSettings>> GetAudioSettingsByMenuId(int menuId);
+        Task<IEnumerable<AudioSettings>> GetAudioSettingsByArea(Area area);
+        Task<IEnumerable<AudioSettings>> GetAudioSettingsByConfigId(int audioConfigId);
+
+        Task<int> UpsertDocumentSetting(UpsertSetting input);
+        Task<int> DeleteDocumentSetting(int documentSettingId);
+        Task<DocumentSettings> GetDocumentSetting(int documentSettingId);
+        Task<IEnumerable<DocumentSettings>> GetDocumentSettings();
+        Task<IEnumerable<DocumentSettings>> GetDocumentSettingsByMenuId(int menuId);
+        Task<IEnumerable<DocumentSettings>> GetDocumentSettingsByArea(Area area);
+        Task<IEnumerable<DocumentSettings>> GetDocumentSettingsByConfigId(int documentConfigId);
     }
 }
