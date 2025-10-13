1ecac0912615f81d2cf1c7a7818f76bf0891cf63
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-13 15:09:06 +0330

    Create UpsertRoleAccessBanned.cs

---
+++ Core/Domain/Models/Dynamicity/UpsertRoleAccessBanned.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OHaraj.Core.Domain.Entities.Management;
+using Project.Application.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace OHaraj.Core.Domain.Models.Dynamicity
+{
+    public class UpsertRoleAccessBanned
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public string RoleId { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public int MenuId { get; set; }
+    }
+}
