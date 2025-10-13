0da87962ddecb4b828a051778f2dd8b5aac9e772
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-13 15:09:09 +0330

    Create UpsertMenu.cs

---
+++ Core/Domain/Models/Dynamicity/UpsertMenu.cs
@@ -0,0 +1,14 @@
+﻿using Project.Application.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace OHaraj.Core.Domain.Models.Dynamicity
+{
+    public class UpsertMenu
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public int? ParentId { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public string Title { get; set; } = string.Empty;
+    }
+}
