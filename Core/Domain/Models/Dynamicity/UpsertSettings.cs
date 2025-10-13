a020f5b358377c3b6e34d69d851dd0dbc1b1ae4d
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-13 20:18:13 +0330

    Create UpsertSettings.cs

---
+++ Core/Domain/Models/Dynamicity/UpsertSettings.cs
@@ -0,0 +1,19 @@
+﻿using OHaraj.Core.Domain.Entities.Configs;
+using OHaraj.Core.Domain.Entities.Management;
+using OHaraj.Core.Enums;
+using Project.Application.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace OHaraj.Core.Domain.Models.Dynamicity
+{
+    public class UpsertSettings
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public Area Area { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public int ConfigId { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public int MenuId { get; set; }
+    }
+}
