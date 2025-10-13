06806bbafc65b40fcf74c8b98b1e57c78200d8cf
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-13 15:24:03 +0330

    Create UpsertVideoConfig.cs

---
+++ Core/Domain/Models/Dynamicity/UpsertVideoConfig.cs
@@ -0,0 +1,16 @@
+﻿using Project.Application.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace OHaraj.Core.Domain.Models.Dynamicity
+{
+    public class UpsertVideoConfig
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public int? MaxSize { get; set; }
+        public int? MaxWidth { get; set; }
+        public int? MaxHeight { get; set; }
+        public int? MaxLength { get; set; }
+    }
+}
