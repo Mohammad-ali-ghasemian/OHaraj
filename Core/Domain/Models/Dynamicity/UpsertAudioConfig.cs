5328f2172ada940eaf6559db9cada474168e3d7c
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-13 15:23:55 +0330

    Create UpsertAudioConfig.cs

---
+++ Core/Domain/Models/Dynamicity/UpsertAudioConfig.cs
@@ -0,0 +1,13 @@
+﻿using Project.Application.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace OHaraj.Core.Domain.Models.Dynamicity
+{
+    public class UpsertAudioConfig
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public int MaxSize { get; set; }
+        public int MaxLength { get; set; }
+    }
+}
