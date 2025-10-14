a5355a54afe3caf3c42a04aff8555fd4325e20f9
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-14 12:35:41 +0330

    Update UpsertMenu.cs

--- Core/Domain/Models/Dynamicity/UpsertMenu.cs
+++ Core/Domain/Models/Dynamicity/UpsertMenu.cs
@@ -6,7 +6,6 @@ namespace OHaraj.Core.Domain.Models.Dynamicity
     public class UpsertMenu
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
         public int? ParentId { get; set; }
         [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
         public string Title { get; set; } = string.Empty;
