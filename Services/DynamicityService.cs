541fcb08948bfb0a6be004e48110d82179a93c1d
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-14 12:32:31 +0330

    initial DynamicityService

--- Services/DynamicityService.cs
+++ Services/DynamicityService.cs
@@ -1,8 +1,326 @@
-﻿using OHaraj.Core.Interfaces.Services;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using OHaraj.Core.Domain.Entities.Configs;
+using OHaraj.Core.Domain.Entities.Management;
+using OHaraj.Core.Domain.Entities.Settings;
+using OHaraj.Core.Domain.Models.Dynamicity;
+using OHaraj.Core.Domain.Models.Dynamicity.Configs;
+using OHaraj.Core.Enums;
+using OHaraj.Core.Interfaces.Repositories;
+using OHaraj.Core.Interfaces.Services;
+using Project.Application.Contracts.Infrastructure;
 
 namespace OHaraj.Services
 {
     public class DynamicityService : IDynamicityService
     {
+        private readonly IDynamicityRepository _dynamicityRepository;
+        private readonly IAuthenticationRepository _authenticationRepository;
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly string FileContainer;
+        private readonly IFileStorageService _uploaderService;
+        public DynamicityService(
+            IDynamicityRepository dynamicityRepository,
+            IAuthenticationRepository authenticationRepository,
+            IMapper mapper,
+            IHttpContextAccessor httpContextAccessor,
+            IFileStorageService uploaderService
+            )
+        {
+            _dynamicityRepository = dynamicityRepository;
+            _authenticationRepository = authenticationRepository;
+            _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+            FileContainer = "Product";
+            _uploaderService = uploaderService;
+        }
+
+        public async Task<IdentityUser> Current()
+        {
+            var userPrincipal = _httpContextAccessor.HttpContext?.User;
+            if (userPrincipal == null)
+            {
+                return null;
+            }
+
+            var user = await _authenticationRepository.GetUserByPrincipalAsync(userPrincipal);
+            return user;
+        }
+
+        public Task<int> DeleteAccessBan(int accessBanId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAudioConfig(int audioConfigId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAudioSetting(int audioSettingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteDocumentConfig(int documentConfigId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteDocumentSetting(int documentSettingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteImageConfig(int imageConfigId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteImageSetting(int imageSettingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteMenu(int menuId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteVideoConfig(int videoConfigId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteVideoSetting(int videoSettingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RoleAccessBanned> GetAccessBan(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<RoleAccessBanned>> GetAccessBan(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<RoleAccessBanned>> GetAllAccessBans()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AudioConfigs> GetAudioConfig(int audioConfigId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AudioConfigs>> GetAudioConfigs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AudioSettings> GetAudioSetting(int audioSettingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AudioSettings>> GetAudioSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AudioSettings>> GetAudioSettingsByArea(Area area)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AudioSettings>> GetAudioSettingsByConfigId(int audioConfigId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AudioSettings>> GetAudioSettingsByMenuId(int menuId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DocumentConfigs> GetDocumentConfig(int documentConfigId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DocumentConfigs>> GetDocumentConfigs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DocumentSettings> GetDocumentSetting(int documentSettingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DocumentSettings>> GetDocumentSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DocumentSettings>> GetDocumentSettingsByArea(Area area)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DocumentSettings>> GetDocumentSettingsByConfigId(int documentConfigId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DocumentSettings>> GetDocumentSettingsByMenuId(int menuId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ImageConfigs> GetImageConfig(int imageConfigId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ImageConfigs>> GetImageConfigs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ImageSettings> GetImageSetting(int imageSettingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ImageSettings>> GetImageSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ImageSettings>> GetImageSettingsByArea(Area area)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ImageSettings>> GetImageSettingsByConfigId(int imageConfigId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ImageSettings>> GetImageSettingsByMenuId(int menuId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Menu> GetMenu(int menuId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Menu>> GetMenus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Menu>> GetMyAccessMenus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Menu>> GetOtherAccessMenus(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VideoConfigs> GetVideoConfig(int videoConfigId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<VideoConfigs>> GetVideoConfigs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VideoSettings> GetVideoSetting(int videoSettingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<VideoSettings>> GetVideoSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<VideoSettings>> GetVideoSettingsByArea(Area area)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<VideoSettings>> GetVideoSettingsByConfigId(int videoConfigId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<VideoSettings>> GetVideoSettingsByMenuId(int menuId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpsertAccessBan(UpsertRoleAccessBanned input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpsertAudioConfig(UpsertAudioConfig input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpsertAudioSetting(UpsertSetting input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpsertDocumentConfig(UpsertDocumentConfig input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpsertDocumentSetting(UpsertSetting input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpsertImageConfig(UpsertImageConfig input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpsertImageSetting(UpsertSetting input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpsertMenu(UpsertMenu input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpsertVideoConfig(UpsertVideoConfig input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpsertVideoSetting(UpsertSetting input)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
