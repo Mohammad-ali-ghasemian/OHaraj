08dfa5bdfe25038d482f3137720d4e8fed00be5c
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-15 22:48:10 +0330

    Update UpsertRoleAccessBanned.cs

--- Core/Domain/Models/Dynamicity/UpsertRoleAccessBanned.cs
+++ Core/Domain/Models/Dynamicity/UpsertRoleAccessBanned.cs
@@ -7,7 +7,7 @@ namespace OHaraj.Core.Domain.Models.Dynamicity
 {
     public class UpsertRoleAccessBanned
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
         public string RoleId { get; set; }
         [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
