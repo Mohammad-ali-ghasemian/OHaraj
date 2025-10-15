0e922da800b747888d7706f32b231f9c70c9dd49
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-15 23:18:17 +0330

    Update UpsertDocumentConfig.cs

--- Core/Domain/Models/Dynamicity/Configs/UpsertDocumentConfig.cs
+++ Core/Domain/Models/Dynamicity/Configs/UpsertDocumentConfig.cs
@@ -5,8 +5,8 @@ namespace OHaraj.Core.Domain.Models.Dynamicity.Configs
 {
     public class UpsertDocumentConfig
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
-        public int? MaxSize { get; set; }
+        public int MaxSize { get; set; }
     }
 }
