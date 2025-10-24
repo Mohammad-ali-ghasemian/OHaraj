480a9661c43f4c7cd3491b8429907ac3621ac4d1
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-24 13:54:08 +0330

    Create TextConfigs.cs

---
+++ Core/Domain/Entities/Configs/TextConfigs.cs
@@ -0,0 +1,19 @@
+﻿using OHaraj.Core.Domain.Entities.Settings;
+
+namespace OHaraj.Core.Domain.Entities.Configs
+{
+    public class TextConfigs
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Font { get; set; }
+        public int? Size { get; set; }
+        public int? Weight { get; set; }
+        public int? Opacity { get; set; }
+        public string? Color { get; set; }
+        public string? BackgroundColor { get; set; }
+
+        // Navigation Properties
+        public List<TextSettings> TextSettings { get; set; }
+    }
+}
