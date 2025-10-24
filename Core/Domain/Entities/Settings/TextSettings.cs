f21d188b4e443bf093e04138956ec6582f953c67
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-24 13:54:12 +0330

    Create TextSettings.cs

---
+++ Core/Domain/Entities/Settings/TextSettings.cs
@@ -0,0 +1,19 @@
+﻿using OHaraj.Core.Domain.Entities.Configs;
+using OHaraj.Core.Domain.Entities.Management;
+using OHaraj.Core.Enums;
+
+namespace OHaraj.Core.Domain.Entities.Settings
+{
+    public class TextSettings
+    {
+        public int Id { get; set; }
+        public Area Area { get; set; }
+
+        // Navigation Properties
+        public TextConfigs TextConfigs { get; set; }
+        public int TextConfigsId { get; set; }
+
+        public Menu Menu { get; set; }
+        public int MenuId { get; set; }
+    }
+}
