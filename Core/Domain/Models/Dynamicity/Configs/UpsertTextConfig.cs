6d22f06a30b248c980e82e117165485fe6d9a8c4
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-24 13:54:29 +0330

    Create UpsertTextConfig.cs

---
+++ Core/Domain/Models/Dynamicity/Configs/UpsertTextConfig.cs
@@ -0,0 +1,19 @@
+﻿using Project.Application.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace OHaraj.Core.Domain.Models.Dynamicity.Configs
+{
+    public class UpsertTextConfig
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public string? Font { get; set; }
+        [Required(ErrorMessage = PublicHelper.RequiredValidationErrorMessage)]
+        public int? Size { get; set; }
+        public int? Weight { get; set; }
+        public int? Opacity { get; set; }
+        public string? Color { get; set; }
+        public string? BackgroundColor { get; set; }
+    }
+}
