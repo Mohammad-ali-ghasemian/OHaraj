f4455932f0b69fbd982370c85a543a9e756d323e
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-28 20:55:12 +0330

    Create MenuDTO.cs

---
+++ Core/Domain/DTOs/MenuDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace OHaraj.Core.Domain.DTOs
+{
+    public class MenuDTO
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Title { get; set; } = string.Empty;
+    }
+}
