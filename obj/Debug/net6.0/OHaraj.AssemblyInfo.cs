b3dd665ba2386edc4774828be447895b328b0127
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-29 02:02:55 +0330

    d

--- obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
+++ obj/Debug/net6.0/OHaraj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca9dce51441694b242ba158a6bc03b3a407a367")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dedfd730e3e25a8391665873a111e135565ac08")]
 [assembly: System.Reflection.AssemblyProductAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OHaraj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
