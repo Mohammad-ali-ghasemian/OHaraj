a31cd2f5f3ba525f52c935194f81a4a85f00efd0
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2025-10-28 23:48:38 +0330

    Create RoleAccessDTO.cs

---
+++ Core/Domain/DTOs/RoleAccessDTO.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OHaraj.Core.Domain.Entities.Management;
+
+namespace OHaraj.Core.Domain.DTOs
+{
+    public class RoleAccessDTO
+    {
+        public int Id { get; set; }
+        public string RoleId { get; set; }
+        public int MenuId { get; set; }
+    }
+}
